8f8f85eeaacd91fc9eff8a13c7c4b63c8c887399
Rob Sterlini <robsterlini@gmail.com>
2015-03-04 10:46:59 +0000

    Created strikethrough functionality and styling
    Signed-off-by: Rob Sterlini <robsterlini@gmail.com>

--- source/assets/js/_browserSniff.js
+++ source/assets/js/_browserSniff.js
@@ -19,6 +19,21 @@ g.browserSniff = function() {
 		if ($(self.selectors.html).hasClass('touch')) {
 			$(self.selectors.html).removeClass('hover');
 		}
+
+		$('.strike').each(function() {
+			var that = $(this),
+				text = that.text(),
+				words = text.split(" "),
+				setText = '';
+			that.empty().attr('title', text);
+			$.each(words, function(i, v) {
+				var setClass = v.length < 2 ? 'tiny' : v.length > 3 ? 'long' : 'short',
+					space = i == (words.length - 1) ? '' : ' ';
+		    setText += "<span class='strike__strikethrough--" + setClass + "'>" + v + space + "</span>";
+			});
+			that.append(setText);
+		});
 	})
+
 
 };
