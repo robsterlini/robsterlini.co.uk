06fbff69427498454a54988101abca15fa16010f
Rob Sterlini <sterlini@fueled.com>
2015-03-10 19:22:12 +0100

    Added a bit of documentation to the age script
    Signed-off-by: Rob Sterlini <sterlini@fueled.com>

--- source/assets/js/_agePosts.js
+++ source/assets/js/_agePosts.js
@@ -40,6 +40,8 @@ g.agePosts = function() {
     }
   }
 
+  // Function to calculate the difference between two dates
+  // taken from http://www.htmlgoodies.com/html5/javascript/calculating-the-difference-between-two-dates-in-javascript.html#fbid=neQipm-K39D
   Date.dateDiff = function(datepart, fromdate, todate) {  
     datepart = datepart.toLowerCase();  
     var diff = todate - fromdate; 
@@ -51,16 +53,29 @@ g.agePosts = function() {
     return Math.floor( diff/divideBy[datepart]);
   }
 
+  // Function that we can call to insert the age into the post
   self.getAge = function() {
+    // Cache the age selector
     var ageItem = $(self.selectors.postAge);
+    // If one exists then we can do the dance:
     if (ageItem.length) {
+      // Set the age as the attribute from the article
+      // whilst replacing the hyphens with slashes so Firefox and Safari recognise it
       var age = ageItem.attr(self.attrs.age).replace(/-/g, "/"),
+        // Set that as a Date()
         ageDate = new Date(age),
+        // Set today’s date as a Date()
         today = new Date(),
+        // Find the difference between the two in weeks
         ageDiff = Date.dateDiff('w', ageDate, today),
+        // Work out which object we need to grab to set the variables
+        // or set it to false if it’s newer than the oldest we care about
         ageAlert = ageDiff > self.variables.old.age ? ageDiff > self.variables.older.age ? ageDiff > self.variables.oldest.age ? self.variables.oldest : self.variables.older : self.variables.old : false;
+      // If it’s proper old like start doing stuff: 
       if (ageAlert) {
+        // Set the message we’re going to add using the age-based variables from the object
         var ageMessage = '<p class="alert--' + ageAlert.alert + '"><strong>' + ageAlert.greeting + '! </strong>This post is ' + ageDiff + ' weeks old; that’s ' + ageAlert.comparison + '. Some of this might be ' + ageAlert.past + ' out of date. You’ve been warned.</p>';
+        // Then stick it all into where we’re putting it (at the beginning)
         $(self.selectors.warning).prepend(ageMessage);
       }
     }
