75cd6da1a861c3e388f87a8f167e6f5014ac21c2
Rob Sterlini <sterlini@fueled.com>
2015-05-18 21:43:41 +0100

    Tweaked the share code to only fire when a Twitter button exists
    Signed-off-by: Rob Sterlini <sterlini@fueled.com>

--- source/assets/js/_share.js
+++ source/assets/js/_share.js
@@ -9,23 +9,34 @@ g.share = function() {
     twitter: '.twitter-popup'
   };
 
-  document.querySelectorAll(self.selectors.twitter)[0].onclick = function() {
-    // Cache the this
-    var that = this,
-      // Set the width and height of the popup window
-      width = 575, height = 400,
-      // Get the top and left values as half the window height and width
-      left = window.innerWidth / 2, top = window.innerHeight / 2,
-      // Get the href value from the button
-      url = that.getAttribute('href');
-      // Set the options based on that bits we grabbed earlier
-      opts='status=1'+',width='+width+',height='+height+',top='+top+',left='+left;
-    // Open the popup with the url we grabbed, and our newly set options
-    window.open(url, 'twitter', opts);
-    // Switch the button words out for thanks
-    that.innerHTML = 'Thanks!';
-    // Stop the button firing as a link should
-    return false;
+  // Create a nodelist of all of the Twitter buttons
+  var twitterButtons = document.querySelectorAll(self.selectors.twitter),
+    // Cache the length in case we need to use it later
+    twitterButtonsLength = twitterButtons.length
+  // Check if we have any of those buttons on this page
+  if (twitterButtonsLength) {
+    // Loop through all of the Twitter buttons we have
+    for (var i = 0, l = twitterButtonsLength; i < l; i++) {
+      // When you click on it
+      twitterButtons[i].onclick = function() {
+        // Cache the this
+        var that = this,
+          // Set the width and height of the popup window
+          width = 575, height = 400,
+          // Get the top and left values as half the window height and width
+          left = window.innerWidth / 2, top = window.innerHeight / 2,
+          // Get the href value from the button
+          url = that.getAttribute('href');
+          // Set the options based on that bits we grabbed earlier
+          opts='status=1'+',width='+width+',height='+height+',top='+top+',left='+left;
+        // Open the popup with the url we grabbed, and our newly set options
+        window.open(url, 'twitter', opts);
+        // Switch the button words out for thanks
+        that.innerHTML = 'Thanks!';
+        // Stop the button firing as a link should
+        return false;
+      };
+    };
   };
 
 };
