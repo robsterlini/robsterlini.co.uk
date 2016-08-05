d8c896203092cbe951b15737d6d715fe38052f89
Rob Sterlini <sterlini@fueled.com>
2016-08-05 11:02:37 +0100

    Squashed commit of the following:
    commit c30ea5b4cd9966b3d0c536e83acc7a3dc2cd20ab
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Thu Aug 4 22:20:40 2016 +0100
    
        Deploy
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 62a820856d2970ffbc4319fe4a363d9d653f16c2
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Thu Aug 4 19:58:58 2016 +0100
    
        Uncommitted work from Wednesday night
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 284f5cbd633f4031fe35f005a357a5a0557e6ecd
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Wed Aug 3 20:38:21 2016 +0100
    
        Okay, actually going home now
    
    commit bd52d4fad54c2a5e731ddf7f9d9dffa3e164287d
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Wed Aug 3 20:33:52 2016 +0100
    
        Time to to go home!
    
    commit 6af0f2620c53d0d142de2b93b543574a5016734a
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue Aug 2 22:17:12 2016 +0100
    
        maybe broke it
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 61a822580e70500a586286dc1d2344e157e2606e
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue Aug 2 18:02:59 2016 +0100
    
        More work
    
    commit b0a20dd2fe625ebe4969528109f606209097a6f0
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue Aug 2 10:41:54 2016 +0100
    
        Work
    
    commit 35b2901c844be5194a154ee503b1181f26a740ed
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Wed Jun 15 21:47:34 2016 +0100
    
        Updates
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit c07f03b56a9454f17e1ad58c2667952c56b6a3d0
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue May 24 22:10:02 2016 +0100
    
        Moar design work
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 576f9c82b2390ebb1f193d57c771b827478b2fbc
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Thu May 19 20:27:58 2016 +0100
    
        Started making the grid work
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 8d88b7b68a1ff358003cc01f883ac4ab5c76947b
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Sun May 8 22:39:44 2016 +0100
    
        Moving onto the blog
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit e2c729d663811553266b76a7c2f7bf6f32078114
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Fri Apr 29 16:58:44 2016 +0100
    
        Grid work
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 7961965d1b21e64a0c7204d8f637fc3daccd4af8
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue Apr 26 22:33:01 2016 +0100
    
        Initial commit
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 87cee27baff8ab0c54da6e0c821c9de9299e464c
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Tue Apr 19 22:58:01 2016 +0100
    
        Much closer to being finished
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit e0b77f7f41958f845b90e4a2b3247f470ebd8bd6
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Sat Apr 16 18:08:43 2016 +0100
    
        Moar design work
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 0a0465d9a63c2608ba9ca15cbd413a320cdba53f
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Sat Apr 16 12:12:44 2016 +0100
    
        Design tweaks –> Purple
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit a8a4d1b289d724e55ba92315ed06798840b57c84
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Thu Feb 11 22:38:46 2016 +0000
    
        Work
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit f5ff397f20d99994290c3c11858a9c3254044319
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Fri Feb 5 09:00:18 2016 +0000
    
        Done the hero header scrolly thingy
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>
    
    commit 86b52a29149d340e62d2aff48bde5276fccebbf5
    Author: Rob Sterlini <sterlini@fueled.com>
    Date:   Fri Feb 5 08:27:59 2016 +0000
    
        Commits and shit
    
        Signed-off-by: Rob Sterlini <sterlini@fueled.com>

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -3,19 +3,12 @@
 //= require _plugins
 //= require _browserSniff
 //= require _cv
-//= require _disqusComments
 //= require _codeHighlight
-//= require _strikethrough
-//= require _onScreenSetup
-//= require _anchorScroll
+//= require _reveal
+//= require _navigation
 //= require _loadHero
 //= require _agePosts
 //= require _share
-//= require _tabs
-//= require _triathlon
-//= require _strava
-//= require _caseStudyHotspur
-//= require _middleplate
 
 domReady(function(event) {
 	// This is all we need in our main.js file - isn't it neat :)
